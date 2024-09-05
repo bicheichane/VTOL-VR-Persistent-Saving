ed243fe4d63330955393abd04ff0c061f499ab98
Bernardo <bernardopinho@gmail.com>
2024-09-05 19:12:59 +0100

    First working version

---
+++ VTOLPersistentSave/Main.cs
@@ -0,0 +1,22 @@
+﻿global using static VTOLPersistentSave.Logger;
+using ModLoader.Framework;
+using ModLoader.Framework.Attributes;
+using System.IO;
+using System.Reflection;
+
+namespace VTOLPersistentSave
+{
+	[ItemId("jehuty.persistentSave")] // Harmony ID for your mod, make sure this is unique
+	public class Main : VtolMod
+	{
+		private void Awake()
+		{
+		}
+
+
+		public override void UnLoad()
+		{
+			// Destroy any objects
+		}
+	}
+}
---
+++ VTOLPersistentSave/QuickLoadFromFile.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using HarmonyLib;
+
+//Based off of https://gist.github.com/ThatGenericName/11ecf30ef45b5fb93eef97d02366f8e7#file-quickloadfromfile-cs
+
+namespace VTOLPersistentSave
+{
+
+	[HarmonyPatch(typeof(MFDQuicksavePage), nameof(MFDQuicksavePage.OnEnable))]
+	public class QuickLoadFromFile
+	{
+
+		public static bool QuicksaveOverride = true;
+
+		/// <summary>
+		/// despite VTOL:VR saving the quicksave to a file, it never attempts to actually load from this file, instead
+		/// only attempting to load data from ram if it exists there.
+		/// This forces the quicksave manager to reload from file if there is no quicksave in ram,
+		/// whenever the MFD Quicksave page is opened.
+		/// </summary>
+		/// <returns>Always returns true as we always want the base quickload logic to run</returns>
+		[HarmonyPrefix]
+		public static bool QuicksaveOverridePrefix()
+		{
+
+			if (!QuicksaveOverride)
+			{
+				Log("Quickload override disabled");
+				return true;
+			}
+
+			Log("In-Memory quicksave override enabled");
+
+			ConfigNode qsn = (ConfigNode)Traverse.Create(typeof(QuicksaveManager)).Field("quicksaveNode").GetValue();
+			
+			if (qsn != null)
+			{
+				Log("Quicksave file already exists in memory, disabling override functionality");
+				QuicksaveOverride = false;
+				return true;
+			}
+
+			Log("Reading quicksave file...");
+			ConfigNode newQsn = ConfigNode.LoadFromFile(Path.Combine(PilotSaveManager.saveDataPath, "quicksave.cfg"), true);
+
+			Log("Overriding quicksave config node");
+			Traverse.Create(typeof(QuicksaveManager)).Field("quicksaveNode").SetValue(newQsn);
+
+			Log("Override attempt successful. Disabling further attempts");
+
+			QuicksaveOverride = false;
+
+			return true;
+		}
+
+	}
+}
